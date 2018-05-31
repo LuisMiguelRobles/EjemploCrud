73a187de9f1e02ddd5e77062f99c551290bb2ea3
LuisMiguelRobles <luismirobles97@gmail.com>
2018-05-31 15:19:34 -0500

    Crud Leer Eliminar

---
+++ js/main.js
@@ -0,0 +1,71 @@
+let users = [];
+$(function () {
+
+
+    /**
+     * Peticion Get al Servidor
+     */
+    $.ajax({
+
+        "url": "https://jsonplaceholder.typicode.com/users",
+        "type": "GET",
+        "data": {},
+        "dataType": "JSON"
+
+
+    }).done(function (data) {
+        users = data;
+        if (data) {
+            renderizar(data);
+        }
+    });
+
+});
+
+
+function renderizar(data) {
+
+
+    let html = `<table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>UserName</td>
+                        <td>Email</td>
+                    </tr>
+                </thead>`;
+
+    $.each(data, function (key, value) {
+        html +=
+            `<tr>
+                <td>${value.name}</td>
+                <td>${value.username}</td>
+                <td>${value.email}</td>
+                <td>
+                    <button class="btn btn-info">Editar</button>
+                    <button class="btn btn-danger delete" id="delete" onclick="eliminar(${value.id})">Eliminar</button>
+                </td>
+            </tr>`;
+    });
+    html += `</table>`;
+    $("#render").html(html);
+
+}
+
+
+function eliminar(id) {
+    let index;
+
+    for (let value of users) {
+        if (value.id == id) {
+            index = users.indexOf(value);
+            users.splice(index, 1);
+            console.log(users);
+
+            renderizar(users);
+
+
+        }
+
+    }
+}
